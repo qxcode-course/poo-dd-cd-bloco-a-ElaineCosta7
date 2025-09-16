08f0ca6471e36a9aff70bf67343f6cb43388b142
ElaineCosta7 <elainetiny616@gmail.com>
2025-09-16 20:24:02 +0000

    acompanhamento da aula explicando conceitos de class e def

---
+++ poo/database/toalha/py/draft.py
@@ -0,0 +1,14 @@
+print('minha toalha é amarela')
+
+class Towel:
+    def __init__(self, color): #construtor
+        self.color = 'yellow' #atributos
+        self.size = 'm'
+        self.wetness = 0
+
+#referencia
+towel = Towel() #objetos
+
+print(towel.color)
+print(towel.size)
+print(towel.wetness)
