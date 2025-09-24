8751f439fe4ed32b839551221a058c4c2eda70ff
ElaineCosta7 <elainetiny616@gmail.com>
2025-09-24 04:48:57 +0000

    continuação da atividade animal, adição da main e outras duas funções

--- poo/database/animal/py/draft.py
+++ poo/database/animal/py/draft.py
@@ -1,8 +1,31 @@
 class Animal:
-    def __init__ (self, species: str, age: int, sound: str):
+    def __init__ (self, species: str, sound: str):
         self.species: str = species
         self.sound: str = sound
         self.age: int = 0
+
+    def ageBy(self, increment: int):
+        self.age += increment
+        if self.age == 0:
+            return "filhote"
+        elif self.age == 1:
+            return "criança"
+        elif self.age == 2:
+            return "adulta"
+        elif self.age == 3:
+            return "idoso"
+        elif self.age == 4:
+            return "morto"
+        elif self.age > 4:
+            print(f"warning: {self.species} morreu")
+
+    def makeSound(self):
+        if self.age == 0:
+            return "---"
+        elif self.age == 4:
+            return "RIP"
+        elif self.age == 1 or self.age == 2 or self.age ==3:
+            return self.sound
 
     def __str__(self) -> str:
         return f"{self.species}:{self.age}:{self.sound}"
@@ -10,5 +33,23 @@ class Animal:
 
 def main():
     animal = Animal("", "")
-    while True
-        line: 
+    while True:
+        line: str = input()
+        print(f"${line}") # eco
+        args: list[str] = line.split(" ")
+        if args[0] == "end":
+            print(f"${line}") # eco
+            break
+        elif args[0] == "init":
+            species = args[1]
+            sound = args[2]
+            animal = Animal(species, sound)
+        elif args[0] == "show":
+            print(animal)
+        elif args[0] == "ageBy":
+            increment: int = int(args[1])
+            animal.ageBy(increment)
+        else:
+            print("fail: comando inválido")
+
+
